85e12bd9e47dff1a9811655b5d2162113f767a8f
shinithp <shinithp78@gmail.com>
2025-06-11 23:01:59 +0530

    course and chapter creation complted

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,89 @@
-import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import test from "node:test";
+
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
+
+const { video } = mux;
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+    if (!ownCourse) return new NextResponse("Unauthorized", { status: 401 });
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      }
+    })
+    if(!chapter) return new NextResponse("Not Found", { status: 404 });
+
+    if(chapter.videoUrl){
+      const existingMuxData = await db.muxData.findFirst({
+          where: {
+            chapterId: params.chapterId,
+          }
+      });
+
+      if(existingMuxData){
+        await video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          }
+        });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId
+      }
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      }
+    });
+
+    if(!publishedChaptersInCourse.length){
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        }
+      })
+    }
+
+    return NextResponse.json(deletedChapter);
+
+  } catch (error) {
+    console.log("Chapterid_Delete", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
 
 export async function PATCH(
   req: Request,
@@ -10,7 +93,7 @@ export async function PATCH(
     const { userId } = auth();
     if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-    const { isPublished , ...values } = await req.json();
+    const { isPublished, ...values } = await req.json();
 
     const ownCourse = await db.course.findUnique({
       where: {
@@ -24,19 +107,50 @@ export async function PATCH(
     }
 
     const chapter = await db.chapter.update({
-        where : {
-            id: params.chapterId,
-            courseId: params.courseId,
-        },
-        data: {
-            ...values,
-        }
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
     });
 
-    //TODO: handle video update
+    //handle video update
+    if (values.videoUrl) {
+      // Check if the video already exists
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await video.assets.create({
+        inputs: [{ url: values.videoUrl }],
+        playback_policies: ["public"],
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
 
     return NextResponse.json(chapter);
-
+    
   } catch (error) {
     console.log("[Chapter Update]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/providers/toaster-provider";
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`antialiased`}>
+          <ConfettiProvider />
           <ToastProvider />
           {children}
         </body>
