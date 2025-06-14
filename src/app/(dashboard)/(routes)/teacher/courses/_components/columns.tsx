bdb0075125c519d7e72ba5e8d0c045251238acee
shinithp <shinithp78@gmail.com>
2025-06-14 11:02:44 +0530

    seach implementation started

--- src/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -3,7 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -33,6 +43,19 @@ export const columns: ColumnDef<Course>[] = [
         </Button>
       );
     },
+    cell: ({ row }) => {
+        const price = parseFloat(row.getValue("price") || "0");
+        const formatted = new Intl.NumberFormat("es-Us", {
+            style: "currency",
+            currency: "USD",
+        }).format(price);
+
+        return (
+            <div>
+                {formatted}
+            </div>
+        )
+    }
   },
   {
     accessorKey: "isPublished",
@@ -47,5 +70,42 @@ export const columns: ColumnDef<Course>[] = [
         </Button>
       );
     },
+    cell: ({ row }) => {
+        const isPublished = row.getValue("isPublished") || false;
+
+        return (
+            <Badge className={cn(
+                "bg-slate-500",
+                isPublished && "bg-sky-700"
+            )}>
+                {isPublished ? "Published" : "Draft"}
+            </Badge>
+        )
+    }
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+        const { id } = row.original;
+
+        return(
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} className="h-4 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="w-4 h-4"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <Link href={`/teacher/courses/${id}`}>
+                        <DropdownMenuItem>
+                            <Pencil className="h-4 w-4 mr-2"/>
+                            Edit
+                        </DropdownMenuItem>
+                    </Link>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        )
+    }
+  }
 ];
